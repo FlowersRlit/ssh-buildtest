9a5442cb939525656ae1d8ae3340ee519efc9104
Chika Kirana <chikakirana15@gmail.com>
2023-04-05 07:32:44 +0700

    routed Router to entry point

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 
 const router = require('./routes/router');
 
+// manually generated
+const userRouter = require('./routes/routerBiodata')
+const exitRouter = require('./routes/routerExit')
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +15,10 @@ app.use(express.json());
 
 app.use(cors({ origin: true, credentials: true }));
 
+// index.js as an entry point, will re-route either
+// every router must have its own directory, not /
 app.use('/', router);
+app.use('/', userRouter);
+app.use('/', exitRouter);
 
-app.listen(process.env.SERVER_PORT, () => {console.log('Server Running')});
+app.listen(process.env.SERVER_PORT, () => {console.log('Server Running ' + process.env.SERVER_PORT)});
